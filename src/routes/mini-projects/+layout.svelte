d10c830f05a49f8801be014697928f9a0cfddb88
Fahad <mohd.fahad@ibm.com>
2025-05-13 23:18:24 +0530

    add mini-project links

---
+++ src/routes/mini-projects/+layout.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	import { base } from '$app/paths';
+	import { page } from '$app/state';
+	let { children } = $props();
+</script>
+
+<div class="grid grid-cols-1 gap-4 md:grid-cols-12 md:gap-4">
+	<div class="col-span-3 md:col-span-3">
+		<div class="container h-full max-w-lg bg-gray-50 p-6">
+			<h1 class="text-3xl text-blue-800">
+				<a href="{base}/mini-projects"> Mini Projects</a>
+			</h1>
+			<ul class="mt-6">
+				<li
+					class="my-4 border border-blue-200 p-2 text-lg"
+					class:border-red-500={page.url.pathname === base + '/mini-projects/email-subscribe'}
+				>
+					<a class="text-blue-500 hover:text-blue-300" href="{base}/mini-projects/email-subscribe">
+						Email Subscribe
+					</a>
+				</li>
+                <li
+					class="my-4 border border-blue-200 p-2 text-lg"
+					class:border-red-500={page.url.pathname === base + '/mini-projects/pricing-grids'}
+				>
+					<a class="text-blue-500 hover:text-blue-300" href="{base}/mini-projects/pricing-grids">
+						Pricing Grids
+					</a>
+				</li>
+                <li
+					class="my-4 border border-blue-200 p-2 text-lg"
+					class:border-red-500={page.url.pathname === base + '/mini-projects/product-modal'}
+				>
+					<a class="text-blue-500 hover:text-blue-300" href="{base}/mini-projects/product-modal">
+						Product Modal
+					</a>
+				</li>
+                <li
+					class="my-4 border border-blue-200 p-2 text-lg"
+					class:border-red-500={page.url.pathname === base + '/mini-projects/image-gallery'}
+				>
+					<a class="text-blue-500 hover:text-blue-300" href="{base}/mini-projects/image-gallery">
+						Image Gallery
+					</a>
+				</li>
+                <li
+					class="my-4 border border-blue-200 p-2 text-lg"
+					class:border-red-500={page.url.pathname === base + '/mini-projects/login-modal'}
+				>
+					<a class="text-blue-500 hover:text-blue-300" href="{base}/mini-projects/login-modal">
+						Login Modal
+					</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div class="col-span-9 md:col-span-9 bg-gray-50">
+		{@render children()}
+	</div>
+</div>
--- src/routes/mini-projects/email-subscribe/+page.svelte
+++ src/routes/mini-projects/email-subscribe/+page.svelte
@@ -1,5 +1,42 @@
+<script>
+	import image from '$lib/mini-projects/email-subscribe/images/image.jpg';
+</script>
+
 <svelte:head>
 	<title>Email Subscribe | Mini Projects</title>
 </svelte:head>
 
-<div class="flex justify-center items-center px-6">Hello</div>
+<!-- Background -->
+<div class="flex h-full items-center justify-center px-6">
+	<!-- Card -->
+	<div class="mx-6 rounded-2xl bg-zinc-800 p-2">
+		<!-- Flex container-->
+		<div class="flex flex-col rounded-l-xl md:flex-row">
+			<!-- Image -->
+			<img
+				src={image}
+				class="object-fit h-80 transform rounded-xl duration-200 hover:scale-105 hover:rounded-xl md:h-64 md:rounded-l-xl md:rounded-r-none"
+				alt=""
+			/>
+		</div>
+		<!-- Content -->
+		<div class="p-6 md:p-12">
+			<h2 class="text-center font-serif text-xl font-medium text-white md:text-left">
+				Get dite and fitness tips in your inbox.
+			</h2>
+			<p class="my-4 max-w-xs text-center text-xs leading-5 tracking-wide text-white md:text-left">
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut facere voluptate voluptatum.
+			</p>
+			<div class="mt-5 flex flex-col space-y-4 md:flex-row md:space-y-0 md:space-x-3">
+				<input
+					type="text"
+					class="border border-zinc-600 bg-zinc-800 p-2 px-4 text-center
+					 text-white  placeholder:text-center
+					  placeholder:text-xs focus:outline-none md:text-left placeholder:md:text-left"
+					placeholder="Enter your email"
+				/>
+				<button class="">Subscribe</button>
+			</div>
+		</div>
+	</div>
+</div>
